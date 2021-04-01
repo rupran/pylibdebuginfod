9c1600f9ee6202d7310796d6ac0472a21e30f587
Andreas Ziegler <andreas.ziegler@fau.de>
2021-04-01 12:32:46 +0200

    setup.py: extend

--- setup.py
+++ setup.py
@@ -2,17 +2,29 @@
 #
 # SPDX-License-Identifier: MIT
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
-    name = 'libdebuginfod-python',
+    name = 'pylibdebuginfod',
     description = 'Python bindings for libdebuginfod',
     author = 'Andreas Ziegler',
     author_email = 'andreas.ziegler@fau.de',
-    url = 'https://github.com/rupran/libdebuginfod-python',
+    url = 'https://github.com/rupran/pylibdebuginfod',
     version = '0.1',
     license = 'MIT',
     packages = [
         'debuginfod'
+    ],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Debuggers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ]
 )
